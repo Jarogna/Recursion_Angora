58441c9fed50972b4a94d47f53ba5806d6caaaf1
Jarogna <>
2021-05-03 05:29:04 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//Factorial function
+int f(int n)
+  {
+   if (n <= 1)
+      return 1;
+   else 
+      return n*f(n-1);
+  }
+
+int main()
+  {
+   int num = 0;
+   cout << "Enter a number: ";
+   cin >> num;
+   cout << "Factorial of entered number: " << f(num);
+   
+   return 0;
+  }
