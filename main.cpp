7e108c87d5b0e1cb989565a4e750dfbae1adfa17
Jarogna <>
2021-05-05 23:03:24 +0000

    Added Input Validation header file.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
+//Simple recursion program
+//Teacher: Dr. Tyson McMillan
+//Student: Ronald Angora
+//Date: 05/03/2021
+
 #include <iostream>
+#include "Input_Validation_Extended.h"
 using namespace std;
 
-//Factorial function
-int f(int n)
-  {
-   if (n <= 1)
-      return 1;
-   else 
-      return n*f(n-1);
-  }
+//Tower of Hanoi Function
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+{
+  
+}
+
+//Factorial Function
+int factorialR(int n)
+{
+  if (n <= 1)
+    return 1;
+  else 
+    return n*factorialR(n-1);
+}
 
 int main()
-  {
-   int num = 0;
-   cout << "Enter a number: ";
-   cin >> num;
-   cout << "Factorial of entered number: " << f(num);
+{
+  int num = 0;
+  cout << "Enter a number: ";
+  num = validateInt(num);
+  cout << "Factorial of entered number: " << factorialR(num);
    
-   return 0;
-  }
+  return 0;
+}
